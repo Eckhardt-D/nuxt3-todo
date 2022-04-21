cfb80abaeb60480cf6f577479861af56d63003cb
Eckhardt <support@kaizenmedia.co.za>
2022-04-21 21:05:14 +0200

    feat: refactor view into pages. Add auth page.

---
+++ pages/auth.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { useUserStore } from '~/store/user';
+</script>
+
+<template>
+  <main class="min-h-screen pt-18 bg-gray-100">
+    <section class="text-center py-10">
+      <h1 class="text-5xl font-bold text-gray-700">Login to your account</h1>
+    </section>
+  </main>
+</template>
